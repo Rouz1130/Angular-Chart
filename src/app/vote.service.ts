4bdc5da90c8a8dc06d732cdf2a83ed90c8dc8925
unknown <Rouz Majlessi>
2018-02-26 00:01:44 -0800

    Add instantiate saveEntry in service to save data to firestore

--- src/app/vote.service.ts
+++ src/app/vote.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore} from 'angularfire2/firestore';
+import { Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class VoteService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  saveEntry(survey): void {
+    this.afs.collection('surveys').valueChanges();
+  }
 
 }
