f8f7c63db6a2a5680ed52f583d18d165f4f92d58
unknown <Rouz Majlessi>
2018-02-25 23:17:37 -0800

    Add initial files, and api for firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyC3ClxPfjuc1v08CuDFDFPmGtab_p5lHI4",
+  authDomain: "itjourney-chart.firebaseapp.com",
+  databaseURL: "https://itjourney-chart.firebaseio.com",
+  projectId: "itjourney-chart",
+  storageBucket: "itjourney-chart.appspot.com",
+  messagingSenderId: "221899370294"
+  }
 };
