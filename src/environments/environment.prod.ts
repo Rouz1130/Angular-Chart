c5e69e7a9e41d37b7c24f0dcfc638edbb0ce0a33
unknown <Rouz Majlessi>
2018-02-25 23:18:19 -0800

    Add firebase api to production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC3ClxPfjuc1v08CuDFDFPmGtab_p5lHI4",
+    authDomain: "itjourney-chart.firebaseapp.com",
+    databaseURL: "https://itjourney-chart.firebaseio.com",
+    projectId: "itjourney-chart",
+    storageBucket: "itjourney-chart.appspot.com",
+    messagingSenderId: "221899370294"
+    }
 };
